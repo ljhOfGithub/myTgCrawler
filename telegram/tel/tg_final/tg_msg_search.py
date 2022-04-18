f68a2f15152821a85736168506438dbed3ec6639
ljhOfGithub <42089575+ljhOfGithub@users.noreply.github.com>
2022-04-18 20:00:15 +0800

    update tg_msg_search.py

--- telegram/tel/tg_final/tg_msg_search.py
+++ telegram/tel/tg_final/tg_msg_search.py
@@ -31,25 +31,45 @@ min_id = 0  # 开启的消息id
 limit = 100000  # 检索的消息数量
 from telethon import TelegramClient
 import traceback
+import logging
 api_id =9063315
 api_hash = '39ee1e156d8f3a1f99a3c4096ef09452'
 # client_list = []
 # client_list.append(client)
+logging.basicConfig(level=logging.DEBUG,#控制台打印的日志级别
+                    filename='search.log',
+                    filemode='w',##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
+                    #a是追加模式，默认如果不写的话，就是追加模式
+                    format=
+                    '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'
+                    #日志格式
+                    )
+import csv
 async def msg(client,username):
     search = 'address'
     messages = client.iter_messages(username, min_id=min_id, reverse=True, limit=limit, search=search)
-    print(messages)
-    try:
-        async for M in messages:
-            print(M)
-    except:
-        traceback.print_exc()
+    # print(messages)
+    filename = '/home/mytg/mycsv/' + username + '.csv'
+    with open(filename,'w',newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['date','message','user_first','user_last'])
+        try:
+            async for M in messages:
+                user = await client.get_entity(M.from_id)
+                writer.writerow([M.date, str(M.message), user.first_name, user.last_name])
+                # print(M)
+
+        except:
+            # traceback.print_exc()
+            logging.debug(str(traceback.format_exc()))
 if __name__ == '__main__':
     client = TelegramClient('search.session', api_id, api_hash)
     client.start()
     with open('username.txt', 'r') as f:
         usernameList = literal_eval(f.read())
     for username in usernameList:
+        myindex = usernameList.index(username)
+        print(myindex)
         client.loop.run_until_complete(msg(client,username))
     # getUsername()
 #     for c in client_list:
@@ -78,11 +98,3 @@ if __name__ == '__main__':
         # print(result.stringify())
 # for message in client.iter_messages(chat,filter=result):
 #     print(message.message)
-# logging.basicConfig(level=logging.DEBUG,#控制台打印的日志级别
-#                     filename='search.log',
-#                     filemode='a',##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
-#                     #a是追加模式，默认如果不写的话，就是追加模式
-#                     format=
-#                     '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'
-#                     #日志格式
-#                     )
